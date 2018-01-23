8166707e64ebda3674dabb9555061c55125d154c
Matthew Paul Gross <MatthewGross@users.noreply.github.com>
2018-01-23 15:11:26 -0500

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='root'
+PROJECTFOLDER='myproject'
+
+# create project folder
+sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+
+# Add ondrej php repos
+sudo add-apt-repository -y ppa:ondrej/php
+
+# update / upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Just in-case
+sudo apt-get install -y software-properties-common python-software-properties
+
+# install apache 2.5 and php 5.5
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+sudo apt-get install php7.1 php7.1-common
+sudo apt-get install php7.1-curl php7.1-xml php7.1-zip php7.1-gd php7.1-mysql php7.1-mbstring
+
+# make php 7.1 default
+sudo a2dismod php5
+sudo a2enmod php7.1
+
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server
+sudo apt-get install php5-mysql
+
+# install phpmyadmin and give password(s) to installer
+# for simplicity I'm using the same password for mysql and phpmyadmin
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get -y install phpmyadmin
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/${PROJECTFOLDER}"
+    <Directory "/var/www/html/${PROJECTFOLDER}">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+service apache2 restart
+
+# install git
+sudo apt-get -y install git
+
+# install zio
+sudo apt-get -y install zip
+
+# install Composer
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
